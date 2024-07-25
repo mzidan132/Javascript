33f645f27cadb967aa6fc686c6d9268b6663c8f7
mzidan132 <136159336+mzidan132@users.noreply.github.com>
2024-07-25 07:44:09 +0600

    Add files via upload

---
+++ es6module.js
@@ -0,0 +1,5 @@
+import {pi,getCircm} from './math.js'
+
+console.log(pi)
+const circm = getCircm(5)
+console.log(circm)
---
+++ math.js
@@ -0,0 +1,6 @@
+
+export const pi = 3.14
+
+export const getCircm = (radius) =>{
+    return 2*pi*radius
+}
